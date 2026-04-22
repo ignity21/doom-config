0711c57ef293aa945d9f1c5e25d5d25e44ca328f
Chris Chen <chrischen3121@gmail.com>
2026-04-22 11:38:12 +0800

    update keybindings

--- config.d.new/keybindings.el
+++ config.d.new/keybindings.el
@@ -24,6 +24,8 @@
 
 ;; F5 prefix
 (map! :prefix cc/run-map-prefix
+      :desc "Quick run shell" "<f5>" #'quickrun-shell
+      :desc "Quick run" "q" #'quickrun
       :desc "Eval buffer or region" "e" #'+eval/buffer-or-region
       :desc "Eval line" "l" #'+eval/line-or-region
       :desc "Send to REPL" "s" #'+eval/send-region-to-repl
