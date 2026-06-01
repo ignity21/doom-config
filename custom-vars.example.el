cc0bc8f500b73d93114494e611060e97c1a0979b
Chris Chen <chrischen3121@gmail.com>
2026-06-01 20:15:12 +0800

    add custom-vars example file

---
+++ custom-vars.example.el
@@ -0,0 +1,19 @@
+;;; -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; custom-vars.el
+
+(setopt
+  user-full-name "Name"
+  user-mail-address "name@example.com")
+
+;; ui
+(setopt
+  cc/mono-font "Hack"
+  cc/unicode-font "Source Han Sans CN"
+  cc/emoji-font "Noto Color Emoji"
+  cc/font-size 16
+  cc/light-ef-theme 'ef-cyprus)
+
+;; checkers
+(setopt
+  ispell-dictionary "en_US"
+  cc/personal-aspell-en-dict "~/dicts/spell-fu/en.pws")
