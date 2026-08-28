239aeac203b7a95159500cef23ed1d74cd674ebc
chrischen <chrischen@ignity.xyz>
2026-08-28 14:39:34 +0800

    chore(cc/dev): drop dead :tools ein config (module not enabled)

--- modules/cc/dev/config.el
+++ modules/cc/dev/config.el
@@ -1,14 +1,6 @@
 ;;; -*- no-byte-compile: t; lexical-binding: t; -*-
 ;;; cc/dev/config.el
 
-;; :tools
-;; ein (jupyter notebook)
-(when (modulep! :tools ein)
-  (after! ein
-    ;; for jupyter-lab, otherwise use "notebook"
-    (setopt ein:jupyter-server-use-subcommand "server")))
-
-;; [Packages]
 ;; Rainbow mode: highlight color string
 (use-package! rainbow-mode
   :hook ((emacs-lisp-mode html-mode css-mode scss-mode) . rainbow-mode)
